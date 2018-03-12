02b551e6ab5109195017717e97042d9e82a56e20
Martin Gammelholm <mag@dis-play.dk>
2018-03-12 09:25:56 +0100

    First Pitforum commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
 import Front from './Front';
-import List from './List';
-import ListLive from './List-live';
-import ItemDetail from './Item-detail';
 
 class App extends Component {
 	render() {
@@ -15,13 +12,6 @@ class App extends Component {
 					<div className="container">
 						<Header />
 						<Route exact={true} path="/" component={Front} />
-						<Route exact={true} path="/item/list" render={(props) => (
-							<List updateItem={this.updateItem} />
-						)}/>
-						<Route exact={true} path="/item/list-live" render={(props) => (
-							<ListLive />
-						)}/>
-						<Route exact={false} path="/item/detail/:itemid" component={ItemDetail}></Route>
 					</div>
 				</BrowserRouter>
 			</div>
